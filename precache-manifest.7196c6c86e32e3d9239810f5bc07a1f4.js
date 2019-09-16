304ff0bc979d62e5e15c51a454d54c2faf43f0f8
kat.bosnic@mccann.com <katbosnic@gmail.com>
2019-09-16 15:11:45 -0400

    Updates

---
+++ precache-manifest.7196c6c86e32e3d9239810f5bc07a1f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad9a1a0393d5407634169cca92381bf",
+    "url": "/katbosnic/find-my-fave-pup.git/index.html"
+  },
+  {
+    "revision": "8a7f625a874df02dc4ca",
+    "url": "/katbosnic/find-my-fave-pup.git/static/css/main.ff430a5a.chunk.css"
+  },
+  {
+    "revision": "04a254cafa3f285448e4",
+    "url": "/katbosnic/find-my-fave-pup.git/static/js/2.6ac360d5.chunk.js"
+  },
+  {
+    "revision": "8a7f625a874df02dc4ca",
+    "url": "/katbosnic/find-my-fave-pup.git/static/js/main.73266bec.chunk.js"
+  },
+  {
+    "revision": "16e25f4174e7c50e411d",
+    "url": "/katbosnic/find-my-fave-pup.git/static/js/runtime~main.09fc8ccd.js"
+  }
+]);
